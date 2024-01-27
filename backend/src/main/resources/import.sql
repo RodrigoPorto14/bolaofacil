65a6d6626988ef234d3c8f009d890bce93c72dfa
rodri <rodrigo.porto1403@gmail.com>
2024-01-26 21:59:23 -0300

    Ajuste na tela inicial

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,4 +1,8 @@
 INSERT INTO tb_user (email, nickname, password) VALUES ('rodrigo@gmail.com', 'Rodrigo', '$2a$10$RxTylgGR/e.3ernvqJxPTOCbRROqLOQB8vpG/y.6mFwX2ylIMt.8G');
+INSERT INTO tb_user (email, nickname, password) VALUES ('larissa@gmail.com', 'Larissa', '$2a$10$RxTylgGR/e.3ernvqJxPTOCbRROqLOQB8vpG/y.6mFwX2ylIMt.8G');
+INSERT INTO tb_user (email, nickname, password) VALUES ('enzo@gmail.com', 'Enzo', '$2a$10$RxTylgGR/e.3ernvqJxPTOCbRROqLOQB8vpG/y.6mFwX2ylIMt.8G');
+INSERT INTO tb_user (email, nickname, password) VALUES ('kaua@gmail.com', 'Kauã', '$2a$10$RxTylgGR/e.3ernvqJxPTOCbRROqLOQB8vpG/y.6mFwX2ylIMt.8G');
+INSERT INTO tb_user (email, nickname, password) VALUES ('ana@gmail.com', 'Ana', '$2a$10$RxTylgGR/e.3ernvqJxPTOCbRROqLOQB8vpG/y.6mFwX2ylIMt.8G');
 
 INSERT INTO tb_league (endpoint, is_custom, name, season, is_active) VALUES ('', true, 'Personalizado', '', true);
 INSERT INTO tb_league (endpoint, is_custom, name, season, is_active) VALUES ('cblol', false, 'CBLOL', '2024-1', true);
@@ -13,3 +17,44 @@ INSERT INTO tb_league (endpoint, is_custom, name, season, is_active) VALUES ('er
 INSERT INTO tb_league (endpoint, is_custom, name, season, is_active) VALUES ('liga-portugal', false, 'Liga Portugal', '2023', true);
 INSERT INTO tb_league (endpoint, is_custom, name, season, is_active) VALUES ('eurocopa', false, 'Eurocopa', '2024', true);
 INSERT INTO tb_league (endpoint, is_custom, name, season, is_active) VALUES ('world-cup', false, 'Copa do Mundo', '2022', true);
+
+INSERT INTO tb_sweepstake (name, is_private, league_id) VALUES ('UCL 23/24', false, 3);
+
+INSERT INTO tb_participant (user_id, sweepstake_id, role, last_access) VALUES (1, 1, 2, NOW());
+INSERT INTO tb_participant (user_id, sweepstake_id, role, last_access) VALUES (2, 1, 0, NOW());
+INSERT INTO tb_participant (user_id, sweepstake_id, role, last_access) VALUES (3, 1, 0, NOW());
+INSERT INTO tb_participant (user_id, sweepstake_id, role, last_access) VALUES (4, 1, 0, NOW());
+INSERT INTO tb_participant (user_id, sweepstake_id, role, last_access) VALUES (5, 1, 0, NOW());
+
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (1,1,451613,1,0);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (2,1,451613,0,1);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (3,1,451613,2,0);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (4,1,451613,1,2);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (5,1,451613,1,1);
+
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (1,1,451615,3,1);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (2,1,451615,3,0);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (3,1,451615,0,1);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (4,1,451615,2,1);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (5,1,451615,1,0);
+
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (1,1,451624,1,2);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (2,1,451624,1,0);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (3,1,451624,1,2);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (4,1,451624,0,3);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (5,1,451624,2,2);
+
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (1,1,451629,0,2);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (2,1,451629,1,1);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (3,1,451629,0,1);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (4,1,451629,0,3);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (5,1,451629,0,0);
+
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (1,1,451648,0,3);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (2,1,451648,0,2);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (3,1,451648,0,1);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (4,1,451648,1,4);
+INSERT INTO tb_external_bet (user_id, sweepstake_id, external_match_id, home_team_score, away_team_score) VALUES (5,1,451648,0,2);
+
+
+
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -6,6 +6,7 @@ const bolaoFacil = require('../img/bolao.png') as string;
 
 const h2Class = "text-left text-sm font-bold";
 const paragraphSize = "text-sm";
+const divClass = ""
 
 
 const Home = () =>
@@ -18,34 +19,48 @@ const Home = () =>
 
             <main className="flex flex-grow items-center p-8 gap-4 flex-col-reverse lg:flex-row lg:justify-evenly ">
 
-                <div className='flex flex-col max-w-[600px] gap-4 items-center'>
-                    <h1 className="text-xl font-bold"> 🚀 Bem-vindo ao BolãoFácil - O Seu Mundo de Palpites Online! 🏆 </h1>
+                <div className='flex flex-col max-w-[600px] gap-4'>
 
-                    <p className={paragraphSize}>
-                        Descubra a emoção de competir e palpitar como nunca antes em nosso universo de bolões online! No BolãoFácil, você tem o poder de criar bolões personalizados ou participar de torneios emocionantes, seja no campo dos esportes tradicionais ou no cenário eletrizante dos e-sports.
-                    </p>
+                    <div>
+                        <h1 className="text-xl font-bold"> 🚀 Bem-vindo ao BolãoFácil - O Seu Mundo de Palpites Online! 🏆 </h1>
+                        <p className={paragraphSize}>
+                            Descubra a emoção de competir e palpitar como nunca antes em nosso universo de bolões online! No BolãoFácil, você tem o poder de criar bolões personalizados ou participar de torneios emocionantes, seja no campo dos esportes tradicionais ou no cenário eletrizante dos e-sports.
+                        </p>
+                    </div>
+                    
+                    <div>
+                        <h2 className={h2Class}> ✨ Crie Seu Próprio Império de Bolões: </h2>
+                        <p className={paragraphSize}>
+                            Seja o arquiteto do seu próprio destino esportivo! Com nossa plataforma intuitiva, você pode criar bolões exclusivos em poucos cliques. Personalize as regras, defina as partidas, e convide seus amigos para embarcarem nessa jornada competitiva ao seu lado.
+                        </p>
+                    </div>
 
-                    <p className={h2Class}> ✨ Crie Seu Próprio Império de Bolões: </p>
-                    <p className={paragraphSize}>
-                        Seja o arquiteto do seu próprio destino esportivo! Com nossa plataforma intuitiva, você pode criar bolões exclusivos em poucos cliques. Personalize as regras, defina as partidas, e convide seus amigos para embarcarem nessa jornada competitiva ao seu lado.
-                    </p>
-
-                    <h2 className={h2Class}> ⚽🎮 Diversão em Todos os Campos: </h2>
-                    <p className={paragraphSize}>
-                        Do gramado ao teclado, suamos todos os detalhes para oferecer uma experiência única em bolões. Palpite nos jogos de futebol mais aguardados, mergulhe no universo dos e-sports e desafie seus amigos em torneios de tirar o fôlego. Aqui, a paixão pelo esporte é universal!
-                    </p>
-
-                    <h2 className={h2Class}> 📈 Acompanhe o Pódio: </h2>
-                    <p className={paragraphSize}>
-                        O BolãoFácil não é apenas sobre palpites, é sobre liderança. Acompanhe em tempo real o ranking, veja como suas previsões se comparam com as dos seus amigos e prove que você é o verdadeiro mestre dos palpites. A disputa pela coroa nunca foi tão emocionante!
-                    </p>
-
-                    <h2 className={h2Class}> 🆓 Totalmente Gratuito, Sem Letras Miúdas: </h2>
-                    <p className={paragraphSize}>
-                        Aqui, a diversão é para todos, e o melhor de tudo, é totalmente gratuito! Sem taxas escondidas, sem surpresas desagradáveis. O BolãoFácil é o seu passaporte para a emoção dos palpites, sem nenhum custo adicional.
-                    </p>
-
-                    <MainButton to="/login" >CRIAR BOLÃO </MainButton>
+                    <div>
+                        <h2 className={h2Class}> ⚽🎮 Diversão em Todos os Campos: </h2>
+                        <p className={paragraphSize}>
+                            Do gramado ao teclado, suamos todos os detalhes para oferecer uma experiência única em bolões. Palpite nos jogos de futebol mais aguardados, mergulhe no universo dos e-sports e desafie seus amigos em torneios de tirar o fôlego. Aqui, a paixão pelo esporte é universal!
+                        </p>
+                    </div>
+                    
+                    <div>
+                        <h2 className={h2Class}> 📈 Acompanhe o Pódio: </h2>
+                        <p className={paragraphSize}>
+                            O BolãoFácil não é apenas sobre palpites, é sobre liderança. Acompanhe em tempo real o ranking, veja como suas previsões se comparam com as dos seus amigos e prove que você é o verdadeiro mestre dos palpites. A disputa pela coroa nunca foi tão emocionante!
+                        </p>
+                    </div>
+                    
+                    <div>
+                        <h2 className={h2Class}> 🆓 Totalmente Gratuito: </h2>
+                        <p className={paragraphSize}>
+                            Aqui, a diversão é para todos, e o melhor de tudo, é totalmente gratuito! Sem taxas escondidas, sem surpresas desagradáveis. O BolãoFácil é o seu passaporte para a emoção dos palpites, sem nenhum custo adicional.
+                        </p>
+                    </div>
+                    
+                    <div className="mx-auto">
+                        <MainButton to="/login" >CRIAR BOLÃO </MainButton>
+                    </div>
+                    
+                    
                 </div>
 
                 <img className='w-[350px] h-[350px] sm:w-[600px] sm:h-[600px]' src={bolaoFacil} alt="Imagem Bolão Fácil" /> 
